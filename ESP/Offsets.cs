28d3cbceed93f1f2d657fbbc2b47547bf2002b63
Cameliuu <kozo8951@gmail.com>
2023-01-05 09:26:32 -0800

    Fix viewmatrix

--- ESP/Offsets.cs
+++ ESP/Offsets.cs
@@ -3,7 +3,7 @@
 public class Offsets
 {
     public static int
-        iViewMatrix = 0x17DFFC,
+        iViewMatrix = 0x17DFFC -0x6c+0x4*16,
         iLocalPlayer = 0x0017E0A8,
         iEntityList = 0x0018AC04,
 
