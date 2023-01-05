12cb3d6ea780225bdc53c7a9b147f802f3708ad8
Cameliuu <kozo8951@gmail.com>
2023-01-05 06:09:59 -0800

    Add Data class along with Entity and ViewMatrix class

---
+++ ESP/Data.cs
@@ -0,0 +1,21 @@
+﻿using System.Numerics;
+
+namespace ESP;
+
+public class Entity
+{
+    public IntPtr baseAdress;
+    public Vector3 headPos,feetPos;
+    public Vector2 viewAngles;
+    public float mag, viewOff;
+    public int health, team, currentAmmo, dead;
+    public string name;
+}
+
+public class ViewMatrix
+{
+    public float m11, m12, m13 ,m14;
+    public float m21, m22, m23, m24;
+    public float m31, m32, m33, m34;
+    public float m41, m42, m43, m44
+}
