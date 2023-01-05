32c1abe5e07a7ac78043b46e8bcfa29b502ce450
Cameliuu <kozo8951@gmail.com>
2023-01-05 06:05:04 -0800

    Add offset class

--- ESP/Offsets.cs
+++ ESP/Offsets.cs
@@ -2,5 +2,17 @@
 
 public class Offsets
 {
-    
+    public static int
+        iViewMatrix = 0x17DFFC,
+        iLocalPlayer = 0x0017E0A8,
+        iEntityList = 0x0018AC04,
+
+        vHead = 0x4,
+        vFeet = 0x28,
+        vAngles = 0x34,
+        vHealth = 0xEC,
+        vName = 0x205,
+        vAlive = 0xB4,
+        vTeam = 0x01B8,
+        vAmmo = 0x140;
 }
